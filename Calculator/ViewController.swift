c0e339bdca6398b6c80e7c74db4a5364ec8698ab
xyz719 <a20131105808@mac23.local>
2016-03-28 21:16:40 +0800

    the new test
    because run error and i doesn’t find the problem ,so i was make a new
    application

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 20131105808 on 16/3/28.
+//  Copyright © 2016年 20131105808. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var screen: UITextField!
+    
+    @IBAction func zero(sender: UIButton) {
+        
+    }
+    
+    @IBAction func one(sender: UIButton) {
+        
+    }
+    
+    @IBAction func two(sender: UIButton) {
+        
+    }
+    
+    @IBAction func three(sender: UIButton) {
+        
+    }
+    
+    @IBAction func four(sender: UIButton) {
+        
+    }
+    
+    @IBAction func five(sender: UIButton) {
+        
+    }
+    
+    @IBAction func six(sender: UIButton) {
+        
+    }
+    
+    @IBAction func seven(sender: UIButton) {
+        
+    }
+    
+    @IBAction func eight(sender: UIButton) {
+        
+    }
+    
+    @IBAction func nine(sender: UIButton) {
+        
+    }
+    
+    @IBAction func point(sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
